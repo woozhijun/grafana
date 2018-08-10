80a132b9dcf76b2b2137ec7564c62a8429297634
wuzhijun <wuzhijun@mobike.com>
2018-08-10 18:04:29 +0800

    fix SERVER_ROOT_URL

--- Docker.run.sh
+++ Docker.run.sh
@@ -11,6 +11,6 @@ docker run -d \
   -p 3000:3000 \
   --name grafana \
   -v grafana-storage:/var/lib/grafana \
-  -e "GF_SERVER_ROOT_URL=http://grafana.monitor.io" \
+  -e "GF_SERVER_ROOT_URL=http://grafana.mobike.io" \
   -e "GF_EXEC_PROD=production" \
   docker.mobike.io/apm/grafana:$tag \
