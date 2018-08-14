2a17163219f812d1b79bb2e855ad6d650b185484
wuzhijun <wuzhijun@mobike.com>
2018-08-14 19:07:49 +0800

    update docker zoneinfo

--- Docker.run.sh
+++ Docker.run.sh
@@ -11,6 +11,7 @@ docker run -d \
   -p 3000:3000 \
   --name grafana \
   -v grafana-storage:/var/lib/grafana \
+  -v /usr/share/zoneinfo:/usr/share/zoneinfo \
   -e "GF_SERVER_ROOT_URL=http://grafana.mobike.io" \
   -e "GF_EXEC_PROD=production" \
   docker.mobike.io/apm/grafana:$tag \
